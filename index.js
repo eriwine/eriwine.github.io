269188120c854a661f60e7521aed77b8803f9c7a
Eric Winebrenner <erwinebrenner@gmail.com>
2018-05-13 20:28:02 -0400

    shaved mario

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var stage;
 const STAGE_WIDTH = 1280;
 const STAGE_HEIGHT = 720;
 const bgScrollSpeed = 0.5;
+var time = 0.0;
 
 const log = console.log;
 const app = new PIXI.Application(STAGE_WIDTH,STAGE_HEIGHT, {backgroundColor: 0x003f5c});
@@ -31,7 +32,7 @@ var background = new PIXI.extras.TilingSprite(bgTexture,w,h);
 container.addChild(background);
 
 var titleStyle = new PIXI.TextStyle({
-    fontFamily: 'Corbel',
+    fontFamily: 'Josefin Sans',
     fontSize: 64,
     fontStyle: '',
     fontWeight: 'bold',
@@ -48,7 +49,7 @@ var titleStyle = new PIXI.TextStyle({
 });
 
 var titleStyle2 = new PIXI.TextStyle({
-    fontFamily: 'Corbel',
+    fontFamily: 'Josefin Sans',
     fontSize: 48,
     fontStyle: '',
     fontWeight: 'bold',
@@ -64,34 +65,41 @@ var titleStyle2 = new PIXI.TextStyle({
     wordWrapWidth: STAGE_WIDTH
 });
 
-var richText = new PIXI.Text('Hello, welcome to my portfolio.', titleStyle);
+var richText = new PIXI.Text('Hello, welcome to my portfolio', titleStyle);
 richText.x = w / 2;
 richText.y = h / 5;
 richText.anchor.set(0.5);
 container.addChild(richText);
 
-var title2 = new PIXI.Text('Made entirely in Pixi.JS', titleStyle2);
+TweenMax.fromTo(richText,1.0,{y: 0}, {y: h / 5, ease:Quad.easeOut});
+TweenMax.fromTo(richText,1.0,{alpha: 0},{alpha: 1});
+
+var title2 = new PIXI.Text('Click Shaved Mario to continue', titleStyle2);
 title2.x = w / 2;
-title2.y = h / 5 + 96;
+title2.y = -48;// h / 5 + 96;
 title2.anchor.set(0.5);
 container.addChild(title2);
 
-var button = PIXI.Sprite.fromImage("Button.png");
+TweenMax.fromTo(title2,0.8,{y: h / 5 + 300}, {y: h / 5 + 112, ease:Back.easeOut, delay:1.25});
+TweenMax.fromTo(title2,0.8,{alpha: 0},{alpha: 1, delay:1.25});
+
+var mario = PIXI.Sprite.fromImage("ShavedMario.png");
+mario.anchor.set(0.5,1.0);
+mario.x = w/2;
+mario.y = h + 1000;
+mario.interactive = true;
+mario.buttonMode = true;
+container.addChild(mario);
+
+var button = PIXI.Sprite.fromImage("CrispyEric.png");
 button.interactive = true;
 button.buttonMode = true;
 button.anchor.set(0.5);
-button.x = w/2;
+button.x = w/10;
 button.y = h/2;
 button.dragOffsetX = 0;
 button.dragOffsetY = 0; 
-container.addChild(button);
-
-var buttonShadow = PIXI.Sprite.fromImage("Button.png");
-//buttonShadow.anchor.set(button.anchor);
-buttonShadow.x = 9;
-buttonShadow.y = 9;
-//buttonShadow.tint = 0x00000044; 
-//button.addChild(buttonShadow);
+//container.addChild(button);
 
 const onDragStart = event => {
   button.data = event.data;
@@ -99,7 +107,6 @@ const onDragStart = event => {
   let pointerPos = event.data.getLocalPosition(button.parent);
   button.dragOffsetX =  button.x - pointerPos.x;
   button.dragOffsetY =  button.y - pointerPos.y;
-  button.scale.set(1.1 + Math.random()*0.3, 1.1+ Math.random()*0.3);
 };
 
 const onDragEnd = event => {
@@ -115,8 +122,8 @@ const onDragMove = event => {
         //button.position = newPosition;// + button.dragOffset;
         button.x = newPosition.x + button.dragOffsetX;
         button.y = newPosition.y + button.dragOffsetY;
-        button.x = Math.max(0, Math.min(STAGE_WIDTH, button.x));
-        button.y = Math.max(0, Math.min(STAGE_HEIGHT, button.y));
+        button.x = Math.max(0, Math.min(w, button.x));
+        button.y = Math.max(0, Math.min(h, button.y));
     }
 };
 
@@ -128,6 +135,14 @@ const onPointerOut = event => {
     button.tint = 0xffffff;
 };
 
+const onMarioHover = event => {
+    TweenMax.fromTo(mario, 0.5,{rotation: 0}, {rotation: Math.PI * 0.05, repeat:-1, yoyo:true});
+;}
+
+const onMarioPointerOut = event => {
+    TweenMax.to(mario, 0.5, {rotation: 0 });
+};
+
 button.on('pointerdown', onDragStart)
 .on('pointerup',onDragEnd)
 .on('pointerupoutside',onDragEnd)
@@ -135,9 +150,18 @@ button.on('pointerdown', onDragStart)
 .on('pointerover',onPointerOver)
 .on('pointerout',onPointerOut)
 
+mario.on('pointerover', onMarioHover)
+.on('pointerout',onMarioPointerOut)
+
+TweenMax.fromTo(mario,0.8,{y: h + 1000}, {ease: Back.easeOut.config(2.0), y: h + 300, delay: 1.0});
+TweenMax.to(mario,0.5,{y: h + 270, repeat:-1, yoyo: true, delay: 1.8});
+
 app.ticker.add((delta)=>{
     background.tilePosition.x -= bgScrollSpeed * delta;
     background.tilePosition.y -= bgScrollSpeed * delta;
+    time+=0.1;
+
+   // mario.y+=Math.sin(time) * 4.0;
 
     if (button.dragging != true)
     {
@@ -146,6 +170,7 @@ app.ticker.add((delta)=>{
     }
     else
     {
+        button.scale.set(1.1 + Math.random()*0.1, 1.1+ Math.random()*0.1);
         //button.rotation += 0.1 * delta;
         
     }
